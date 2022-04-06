18f0aa75178cb9c64aa4c5c55330e670e2e5a69c
Luka-kuerzdoerfer <103108552+Luka-kuerzdoerfer@users.noreply.github.com>
2022-04-06 13:21:33 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Chessboard)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Butterfly)
+    basic.showIcon(IconNames.No)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # # # # #
+        # . # . #
+        # # # # #
+        `)
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Chessboard)
+})
